57a3c445fb22fb66fc689390d3459ab56c2ef09d
AlexLundin <aml140830@utdallas.edu>
2019-04-24 00:57:55 -0500

    test cases OS independent
    reworked test cases to be OS independent with paths
    reworked test cases to do file ops inside GitHub repo
    reworked test cases to delete files created during tests at the end

--- webapp/tests/test_file_operations.py
+++ webapp/tests/test_file_operations.py
@@ -1,24 +1,34 @@
 from django.test import TestCase
 from file_operations import *
+from refactored_test_methods import *
 import os
 import shutil
+import sys
 
+# Start of Test Cases
 
-# Create your tests here.
+# perform setup tasks for all Tests in this file
+# ensure all old files are deleted before starting Test
+def test_set_directory_environment():
+	# Delete directories if they exist
+    delete_temp_dirs()
+	   
 def test_move():
-   source = os.path.join('C:', os.sep, 'Users', 'Alex', 't.txt')
-   # open blank file at source location
-   f= open(source,"w+")
-   # close blank file
-   f.close()
-   destination = os.path.join('C:', os.sep, 'Users', 'Alex', 'Desktop', 't.txt')
-   shutil.move(source, destination)
+    returned_list = create_t_txt_in_tmp_dir()
+    source = returned_list[0]
+    destination = returned_list[1]
+    shutil.move(source, destination)
+    delete_temp_dirs()
 
 def test_copy():
-   source = os.path.join('C:', os.sep, 'Users', 'Alex', 't.txt')
-   # open blank file at source location
-   f= open(source,"w+")
-   # close blank file
-   f.close()
-   destination = os.path.join('C:', os.sep, 'Users', 'Alex', 'Desktop', 't.txt')
-   copy_file(source, destination)
+    returned_list = create_t_txt_in_tmp_dir()
+    source = returned_list[0]
+    destination = returned_list[1]
+    copy_file(source, destination)
+    delete_temp_dirs()
+
+# perform cleanup tasks after Test case completion
+# ensures conscientious cleanup
+def test_cleanup():
+	# Delete directories if they exist
+    delete_temp_dirs()
