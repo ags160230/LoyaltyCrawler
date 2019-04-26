2a00d9a67b5764cf13d2305bc0d8c43535b00165
AlexLundin <aml140830@utdallas.edu>
2019-04-26 13:46:35 -0500

    test_driver update
    added line to include -s flag to allow printing to console

--- webapp/test_driver.bat
+++ webapp/test_driver.bat
@@ -2,7 +2,9 @@ REM double click to execute all tests in the /tests subfolder
 
 REM turn off command echo
 @echo off
-REM execute python command pytest with the tests subfold as the path
+REM execute python command pytest with the tests subfolder as the path
 python -m pytest tests/
+REM execute python command pytest with the tests subfolder as the path and -s flag to allow functiong printing
+python -m pytest -s tests/
 REM pause after execution for user to see results
 timeout /t -1
